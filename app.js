882ba728abe6286ee426e11263cc37bcfcc8394c
Ciro Araujo <ciroaraujodesantanna@gmail.com>
2023-11-01 21:37:33 -0300

    Incompleto

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ async function updateTable(){
         for(let user of users) {
             linhas +=  `
             <tr>
-                        <td>$(user.name)</td>
-                        <td>$(user.username)</td>
-                        <td>$(user.email)</td>
+                        <td>${user.name}</td>
+                        <td>${user.username}</td>
+                        <td>${user.email}</td>
                         <td>
-                            <button >Exibir</button>   
+                            <button onclick="userDetail()" >Exibir</button>   
                         </td>
                         
             </tr>
@@ -32,8 +32,31 @@ async function updateTable(){
         }
         tableBody.innerHTML = linhas
         console.log(users)
-
-    }
     
+    }
+    async function userDetail(userid){
+        const resp =await fetch('https://jsonplaceholder.typicode.com/users')
+        const user =await resp.json();
+        
+        document.querySelector(".js-user-name").value = user.name   
+        document.querySelector(".js-user-username").value = user.username
+    }
+
     updateTable()
+    async function getTips() {
+        const resp = await fetch("https://api.adviceslip.com/advice")
+        const tip = await resp.json()
+    }
+
+    setTimeout(() => {
+        document.querySelector(".tips").innerText = tip.slip.advice
+        document.querySelector(".tips").style.display = "inline-block"
+        
+    }, 5000);
+    setInterval(() => {
+        getTips()
+        console.log('teste')
+        
+    }, 8000);
+    
     
